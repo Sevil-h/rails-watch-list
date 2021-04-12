ad8d1fca04e934031e0a1de9eb7b3413606c5d7f
sevil hatipoglu <sevilhatipogluu93@gmail.com>
2021-04-12 14:36:24 +0300

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_203552) do
+ActiveRecord::Schema.define(version: 2021_04_12_113237) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_203552) do
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "overview"
-    t.string "porter_url"
+    t.string "poster_url"
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
