d39f5cbbe497f1c3c89a1ae2c4abc4fe3cfbfbc4
sevil hatipoglu <sevilhatipogluu93@gmail.com>
2021-04-15 04:09:24 +0300

    Added js for navbar

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import { initUpdateNavbarOnScroll } from '../components/navbar';
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('turbolinks:load', () => {
+  initUpdateNavbarOnScroll();
+});
+
